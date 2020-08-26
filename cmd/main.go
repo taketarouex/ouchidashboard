d0be96cc2a0996bfe2656e02f969c13d9fa5e0df
TAKESHI OHNUKI <32025300+tktkc72@users.noreply.github.com>
2020-08-26 21:05:55 +0900

    Firestore (#4)
    * repository firestore 実装
    
    * main実装

--- cmd/main.go
+++ cmd/main.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"context"
+	"log"
 	"os"
 
+	"cloud.google.com/go/firestore"
+	"github.com/tenntenn/natureremo"
 	"github.com/tktkc72/ouchi-dashboard/collector"
 )
 
 func main() {
 	accessToken := os.Getenv("ACCESS_TOKEN")
 	deviceID := os.Getenv("DEVICE_ID")
-	fetcher := collector.NewFetcher(accessToken, deviceID)
-	repository := collector.NewRepository()
+	projectID := os.Getenv("GCP_PROJECT")
+	documentPath := os.Getenv("FIRESTORE_DOC_PATH")
+
+	natureremoClient := natureremo.NewClient(accessToken)
+	fetcher := collector.NewFetcher(natureremoClient, deviceID)
+
+	ctx := context.Background()
+	firestoreClient, err := firestore.NewClient(ctx, projectID)
+	if err != nil {
+		log.Fatalf("failed get firestore client due to: %v", err)
+	}
+	repository := collector.NewRepository(firestoreClient, documentPath)
 
 	service := collector.NewCollectorService(fetcher, repository)
-	service.Collect()
+	err = service.Collect()
+	if err != nil {
+		log.Fatalf("failed collect log due to: %v", err)
+	}
 }
