fe4288aef143c02ae17d099b51b762682fad0a50
tktkc72 <32025300+tktkc72@users.noreply.github.com>
2020-09-05 17:36:32 +0900

    テストデータを準備する

--- collector/collector_integration_test.go
+++ collector/collector_integration_test.go
@@ -11,7 +11,7 @@ import (
 	"cloud.google.com/go/firestore"
 )
 
-func TestRepository(t *testing.T) {
+func TestRepository_add(t *testing.T) {
 	projectID := os.Getenv("GCP_PROJECT")
 	sourceID := os.Getenv("NATURE_REMO_DEVICE_ID")
 	rootPath := os.Getenv("FIRESTORE_ROOT_PATH")
@@ -19,13 +19,19 @@ func TestRepository(t *testing.T) {
 	ctx := context.Background()
 	client, err := firestore.NewClient(ctx, projectID)
 	if err != nil {
-		t.Errorf("cant create client because of:%v", err)
+		t.Fatalf("failed to create client because of:%v", err)
 	}
 	defer client.Close()
 
+	// setup test data
+	doc, _, err := client.Collection(rootPath).Add(ctx, map[string]string{"sourceID": sourceID})
+	if err != nil {
+		t.Fatalf("failed to create test data")
+	}
+
 	repository, err := NewRepository(client, rootPath, sourceID)
 	if err != nil {
-		t.Errorf("cant create repository because of: %v", err)
+		t.Fatalf("failed to create repository because of: %v", err)
 	}
 	collectLogs := []collectLog{
 		{0, time.Date(2020, 7, 31, 0, 0, 0, 0, time.Local), temperature, "test"},
@@ -33,8 +39,13 @@ func TestRepository(t *testing.T) {
 		{2, time.Date(2020, 7, 31, 0, 0, 0, 0, time.Local), illumination, "test"},
 		{3, time.Date(2020, 7, 31, 0, 0, 0, 0, time.Local), motion, "test"},
 	}
-	err = repository.add(collectLogs)
-	if err != nil {
-		t.Errorf("error: %v", err)
+
+	if err = repository.add(collectLogs); err != nil {
+		t.Fatalf("error: %v", err)
+	}
+
+	// delete test data
+	if _, err = doc.Delete(ctx); err != nil {
+		t.Fatalf("failed to delete test data")
 	}
 }
--- e2e_test/e2e_test.go
+++ e2e_test/e2e_test.go
@@ -2,19 +2,37 @@ package e2e_test
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"net/http"
 	"os"
 	"testing"
+
+	"cloud.google.com/go/firestore"
 )
 
 type Request struct {
 	DeviceIDs []string `json:"deviceIDs"`
 }
 
-func TestE2E(t *testing.T) {
-	func() {
-		deviceID := os.Getenv("NATURE_REMO_DEVICE_ID")
+func TestCollector_E2E(t *testing.T) {
+	projectID := os.Getenv("GCP_PROJECT")
+	rootPath := os.Getenv("FIRESTORE_ROOT_PATH")
+	deviceID := os.Getenv("NATURE_REMO_DEVICE_ID")
+
+	ctx := context.Background()
+	client, err := firestore.NewClient(ctx, projectID)
+	if err != nil {
+		t.Fatalf("failed to create client because of:%v", err)
+	}
+	defer client.Close()
+	// setup test data
+	doc, _, err := client.Collection(rootPath).Add(ctx, map[string]string{"sourceID": deviceID})
+	if err != nil {
+		t.Fatalf("failed to create test data")
+	}
+
+	t.Run("success", func(t *testing.T) {
 		request := Request{
 			DeviceIDs: []string{deviceID},
 		}
@@ -31,8 +49,8 @@ func TestE2E(t *testing.T) {
 		if resp.StatusCode != 200 {
 			t.Errorf("status: %s", resp.Status)
 		}
-	}()
-	func() {
+	})
+	t.Run("fail invalid deviceID", func(t *testing.T) {
 		deviceID := os.Getenv("NATURE_REMO_DEVICE_ID")
 		request := Request{
 			DeviceIDs: []string{deviceID, "test"},
@@ -47,8 +65,13 @@ func TestE2E(t *testing.T) {
 		}
 
 		defer resp.Body.Close()
-		if resp.StatusCode != 400 {
+		if resp.StatusCode != 500 {
 			t.Errorf("status: %s", resp.Status)
 		}
-	}()
+	})
+
+	// delete test data
+	if _, err = doc.Delete(ctx); err != nil {
+		t.Fatalf("failed to delete test data")
+	}
 }
