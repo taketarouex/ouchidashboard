84fc48e0fe2bba0c5ff71f484323da20b3ea85b4
TAKESHI OHNUKI <32025300+tktkc72@users.noreply.github.com>
2020-09-05 17:49:11 +0900

    Separate out log by device id (#16)
    * deviceIDから保存するfirestore documentを変えるように変更
    
    * テストデータを準備する
    
    * docPathからrootPathを指定するように変更
    
    * テスト関数名変更
    
    * テスト関数更新

--- collector/collector_integration_test.go
+++ collector/collector_integration_test.go
@@ -11,26 +11,41 @@ import (
 	"cloud.google.com/go/firestore"
 )
 
-func TestRepository(t *testing.T) {
+func TestRepository_add(t *testing.T) {
 	projectID := os.Getenv("GCP_PROJECT")
-	documentPath := os.Getenv("FIRESTORE_DOC_PATH")
+	sourceID := os.Getenv("NATURE_REMO_DEVICE_ID")
+	rootPath := os.Getenv("FIRESTORE_ROOT_PATH")
 
 	ctx := context.Background()
 	client, err := firestore.NewClient(ctx, projectID)
 	if err != nil {
-		t.Errorf("cant create client because of:%v", err)
+		t.Fatalf("failed to create client because of:%v", err)
 	}
 	defer client.Close()
 
-	repository := NewRepository(client, documentPath)
+	// setup test data
+	doc, _, err := client.Collection(rootPath).Add(ctx, map[string]string{"sourceID": sourceID})
+	if err != nil {
+		t.Fatalf("failed to create test data")
+	}
+
+	repository, err := NewRepository(client, rootPath, sourceID)
+	if err != nil {
+		t.Fatalf("failed to create repository because of: %v", err)
+	}
 	collectLogs := []collectLog{
 		{0, time.Date(2020, 7, 31, 0, 0, 0, 0, time.Local), temperature, "test"},
 		{1, time.Date(2020, 7, 31, 0, 0, 0, 0, time.Local), humidity, "test"},
 		{2, time.Date(2020, 7, 31, 0, 0, 0, 0, time.Local), illumination, "test"},
 		{3, time.Date(2020, 7, 31, 0, 0, 0, 0, time.Local), motion, "test"},
 	}
-	err = repository.add(collectLogs)
-	if err != nil {
-		t.Errorf("error: %v", err)
+
+	if err = repository.add(collectLogs); err != nil {
+		t.Fatalf("error: %v", err)
+	}
+
+	// delete test data
+	if _, err = doc.Delete(ctx); err != nil {
+		t.Fatalf("failed to delete test data")
 	}
 }
