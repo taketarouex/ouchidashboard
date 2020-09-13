4d704f7d5bda6e32a8694228525db38e88b53b4f
tktkc72 <32025300+tktkc72@users.noreply.github.com>
2020-09-13 16:03:59 +0900

    repository Fetch 実装

--- go.mod
+++ go.mod
@@ -6,7 +6,7 @@ require (
 	cloud.google.com/go v0.65.0 // indirect
 	cloud.google.com/go/firestore v1.3.0
 	github.com/golang/mock v1.4.4
-	github.com/google/go-cmp v0.5.2 // indirect
+	github.com/google/go-cmp v0.5.2
 	github.com/pkg/errors v0.9.1
 	github.com/tenntenn/natureremo v0.0.1
 	golang.org/x/net v0.0.0-20200904194848-62affa334b73 // indirect
--- repository/repository.go
+++ repository/repository.go
@@ -6,25 +6,24 @@ import (
 	"time"
 
 	"cloud.google.com/go/firestore"
+	"github.com/pkg/errors"
 	"github.com/tktkc72/ouchi/collector"
+	"github.com/tktkc72/ouchi/enum"
+	"github.com/tktkc72/ouchi/ouchi"
 )
 
 type (
 	// Repository data store
 	Repository struct {
-		documentRef  *firestore.DocumentRef
-		documentSnap *firestore.DocumentSnapshot
-		time         collector.TimeInterface
-	}
-	ouchiLog struct {
-		Value     float64
-		UpdatedAt time.Time
-		CreatedAt time.Time
+		rootCollection *firestore.CollectionRef
+		documentRef    *firestore.DocumentRef
+		documentSnap   *firestore.DocumentSnapshot
+		time           collector.TimeInterface
 	}
 )
 
 // NewRepository creates repository which has the name specified
-func NewRepository(client *firestore.Client, rootPath, roomName string, time collector.TimeInterface) (collector.IRepository, error) {
+func NewRepository(client *firestore.Client, rootPath, roomName string, time collector.TimeInterface) (ouchi.IRepository, error) {
 	ctx := context.Background()
 	ref := client.Collection(rootPath).Doc(roomName)
 	snap, err := ref.Get(ctx)
@@ -32,12 +31,13 @@ func NewRepository(client *firestore.Client, rootPath, roomName string, time col
 		return nil, err
 	}
 	if !snap.Exists() {
-		return nil, &collector.NoRoomErr{S: fmt.Sprintf("no room name: %s", roomName)}
+		return nil, &ouchi.NoRoomErr{S: fmt.Sprintf("no room name: %s", roomName)}
 	}
 	return &Repository{
-		documentRef:  ref,
-		documentSnap: snap,
-		time:         time,
+		rootCollection: client.Collection(rootPath),
+		documentRef:    ref,
+		documentSnap:   snap,
+		time:           time,
 	}, nil
 }
 
@@ -54,10 +54,10 @@ func (r *Repository) SourceID() (string, error) {
 func (r *Repository) Add(collectLogs []collector.CollectLog) error {
 	ctx := context.Background()
 	for _, c := range collectLogs {
-		o := ouchiLog{
-			c.Value,
-			c.UpdatedAt,
-			r.time.Now(),
+		o := ouchi.Log{
+			Value:     c.Value,
+			UpdatedAt: c.UpdatedAt,
+			CreatedAt: r.time.Now(),
 		}
 		_, _, err := r.documentRef.Collection(c.LogType.String()).Add(ctx, o)
 		if err != nil {
@@ -66,4 +66,49 @@ func (r *Repository) Add(collectLogs []collector.CollectLog) error {
 	}
 
 	return nil
+}
+
+// Fetch fetches logs from repository
+func (r *Repository) Fetch(roomName string,
+	logType enum.LogType,
+	start, end time.Time,
+	limit int, order enum.Order) ([]ouchi.Log, error) {
+	roomDoc := r.rootCollection.Doc(roomName)
+	if roomDoc == nil {
+		return nil, &ouchi.NoRoomErr{S: fmt.Sprintf("no room name: %s", roomName)}
+	}
+	collection := roomDoc.Collection(logType.String())
+	if collection == nil {
+		return nil, errors.Errorf("no collection type: %s", logType.String())
+	}
+
+	direction := firestore.Asc
+	if order == enum.Desc {
+		direction = firestore.Desc
+	}
+
+	query := collection.Where("UpdatedAt", ">=", start).Where("UpdatedAt", "<", end).OrderBy("UpdatedAt", direction)
+	if limit > 0 {
+		query = query.Limit(limit)
+	}
+	ctx := context.Background()
+	docs, err := query.Documents(ctx).GetAll()
+	if err != nil {
+		return nil, errors.Errorf("failed to get documents due to: %v", err)
+	}
+
+	return parse(docs), nil
+}
+
+func parse(docs []*firestore.DocumentSnapshot) []ouchi.Log {
+	parsed := []ouchi.Log{}
+	for _, doc := range docs {
+		log := ouchi.Log{
+			Value:     doc.Data()["Value"].(float64),
+			UpdatedAt: doc.Data()["UpdatedAt"].(time.Time),
+			CreatedAt: doc.Data()["CreatedAt"].(time.Time),
+		}
+		parsed = append(parsed, log)
+	}
+	return parsed
 }
